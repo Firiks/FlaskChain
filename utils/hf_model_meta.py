917a0c66d73f4676788dd84752d5bca57270255a
Michal <michalk12456@gmail.com>
2024-03-07 10:07:05 +0100

    add get_model_card

--- utils/hf_model_meta.py
+++ utils/hf_model_meta.py
@@ -1,11 +1,31 @@
+"""
+Get metadata from Huggingface model hub
+"""
+
 from huggingface_hub import HfApi
+from huggingface_hub import ModelCard
 
 api = HfApi()
 
-def search_model(search: str):
+def search_model(search: str, limit: int = 5):
     global api
 
     models = api.list_models(
         search=search,
+        limit=5
     )
+
     return models
+
+def get_model_card(model_id: str):
+    model_card = ModelCard.load(model_id)
+
+    return model_card
+
+if __name__ == "__main__":
+    models = search_model("TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF")
+
+    for model in models:
+        print(model)
+
+    print(get_model_card("TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF"))
